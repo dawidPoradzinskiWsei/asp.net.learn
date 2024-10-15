fb2ad712a2ffcf4aa3f9bf91ed4cd00f1b1b38a4
dawid <dawid.poradznski@microsoft.wsei.edu.pl>
2024-10-15 17:08:57 +0200

    test

--- Lab1_ASP.NET/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Lab1_ASP.NET.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ MoreAdvancedCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MoreAdvancedCalculator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
