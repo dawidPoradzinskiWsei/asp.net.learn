054ab6bbaa92b8a84f175b03fb913facbc84252b
= <=>
2025-01-02 13:33:35 +0100

    init commit for final project

--- ViewsGetter/Controllers/OrganizationController.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-public class OrganzationController : Controller
-{
-    
-}
--- ViewsGetter/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ViewsGetter.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
